722389f963f5ccb87a2287f7dcc1206001820184
yurys <yury.salyatov@gmail.com>
2025-10-16 14:03:10 +0300

    probably zero org fix

--- graph_builder.py
+++ graph_builder.py
@@ -113,8 +113,9 @@ class GraphBuilder:
             probably_orgs = org_edges[:, org_mask]
             target_orgs = probably_orgs[1, torch.randint(0, probably_orgs.size(1), (num_duplicates,))]
             # TODO maybe not only 1 org, but range(1, len(orgs))?
-            new_authors_org_edges = torch.stack([torch.tensor(duplicate_ind[1:], dtype=torch.long), target_orgs])
-            new_authors_org = torch.cat((new_authors_org, new_authors_org_edges), dim=1)
+            if target_orgs.size(0) > 0:
+                new_authors_org_edges = torch.stack([torch.tensor(duplicate_ind[1:], dtype=torch.long), target_orgs])
+                new_authors_org = torch.cat((new_authors_org, new_authors_org_edges), dim=1)
             author_pairs_mask = torch.isin(paper_edges[0], orig_author_idx)
             author_paper_idx = paper_edges[:, author_pairs_mask]
             for i in range(author_paper_idx.shape[1] // (num_duplicates + 1)):
